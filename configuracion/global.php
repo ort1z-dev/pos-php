5c23155e2e5f194e2b22eb22826e1de2491abc00
ort1zd3v <isaac.ortiz@copixil.com>
2024-12-10 10:34:05 -0600

    Starting structure

---
+++ configuracion/global.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "dbsistema");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+define("DB_ENDCODE", "uft8");
+define("SISTEMA_NOMBRE", "POS_SYSTEM");
+
+
+?>
